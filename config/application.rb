8a4bb22dba66e71e7cd8a726cfaa704e88a72d71
Katharina Kuhn <katykuhndev@gmail.com>
2019-10-01 03:08:43 -0300

    Se cambia timezone a santiago y otros arreglos
    Se cambia largo de willpaginate en production
    se cambia fecha venc por fecha receta en show de rec
    Se deshabilitan radio buttons al estar la rec cerrada

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Pharmacare
     config.load_defaults 5.2
     config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
+    config.time_zone = 'America/Santiago'
+    config.active_record.default_timezone = :local 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
